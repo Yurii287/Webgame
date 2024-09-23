50a19eb4f315a370c94cea065269748ed8c33009
Ben Zadorian <124893639+Yurii287@users.noreply.github.com>
2024-09-23 21:17:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let userData = [];
+
+// Leaderboard Page Elements
+const leaderboardEl = document.getElementById("leaderboard-el");
+
+// Create an Account Elements
+let createUsernameEl = document.getElementById("createName-el");
+// Homepage Elements
+
+// Login Page Elements
+const loginBtn = document.getElementById("login-btn");
+const usernameEl = document.getElementById("username-el");
+const passwordEl = document.getElementById("password-el");
+
+function foo() {
+    console.log(createUsernameEl.value)
+}
